ac0042f0fc677189886edd2492bf81875a559547
Tim Sandberg <tasandberg@gmail.com>
2023-07-25 20:10:37 -0700

    WIP routing, redirect, weirdness. WIP Route hierarchy

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,21 +1,32 @@
+import { Outlet, useNavigate } from "react-router-dom"
 import { useAuth } from "../contexts/auth/AuthContext"
-import icon from "../images/icon.svg"
-import { Button, FloatingLabel, Form } from "react-bootstrap"
 
-function App() {
+import { Button, Container } from "react-bootstrap"
+import { useEffect } from "react"
+
+export default function App() {
   const {
     state: { user },
     dispatch,
   } = useAuth()
 
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (!user) {
+      console.log("user falsey")
+
+      return navigate("/login")
+    }
+  }, [user, navigate])
+
   return (
-    <>
-      <h1>You are logged in {user?.email}</h1>
+    <Container className="p-3 text-center">
+      <h1>You are logged in '{user?.email}'</h1>
       <Button onClick={() => dispatch({ type: "SET_USER", data: undefined })}>
         Clear user
       </Button>
-    </>
+      <Outlet />
+    </Container>
   )
 }
-
-export default App
--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,20 @@ import App from "./components/App.tsx"
 import "./styles/index.scss"
 import { RouterProvider, createBrowserRouter } from "react-router-dom"
 import AuthProvider from "./contexts/auth/AuthProvider.tsx"
+import Login from "./components/Login.tsx"
+import ErrorPage from "./components/ErrorPage.tsx"
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "login",
+        element: <Login />,
+      },
+    ],
   },
 ])
 
