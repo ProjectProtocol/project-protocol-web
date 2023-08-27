b91afa40571657b2e45cdca71183d15d28b4dc86
Tim Sandberg <tasandberg@gmail.com>
2023-08-27 10:39:03 -0700

    Change to default .prettierrc file

--- prettierrc.cjs
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  tabWidth: 2,
-  semi: false,
-  singleQuote: true,
-  printWidth: 120,
-  bracketSpacing: true,
-}
--- src/components/AgentInfo.tsx
+++ src/components/AgentInfo.tsx
@@ -1,4 +1,4 @@
-import Agent from "../types/Agent"
+import Agent from '../types/Agent'
 
 interface IAgentInfo {
   /** An `Agent` object */
