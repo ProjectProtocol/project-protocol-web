12fd9fea33b1b220971c32101ddc7148dd40e97a
Eddie M. Menefee <eddie.m.menefee@gmail.com>
2023-08-29 10:18:13 -0700

    Update story values

--- src/stories/RatingBar.stories.tsx
+++ src/stories/RatingBar.stories.tsx
@@ -13,19 +13,19 @@ type Story = StoryObj<typeof RatingBar>
 
 export const Integer: Story = {
   args: {
-    rating: { label: 'Caring', value: 2.5 },
+    rating: { label: 'Caring', value: 2 },
   },
 }
 
 export const Decimal: Story = {
   args: {
-    rating: { label: 'Caring', value: 2.5 },
+    rating: { label: 'Caring', value: 3.5 },
   },
 }
 
 export const Animated: Story = {
   args: {
-    rating: { label: 'Caring', value: 2.5 },
+    rating: { label: 'Caring', value: 4 },
     animated: true,
     delay: 1,
   },
