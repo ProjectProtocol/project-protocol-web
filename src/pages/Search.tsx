b48dbc05af81ef2dcaead529ce58c3acef6e40d7
Eddie M. Menefee <eddie.m.menefee@gmail.com>
2023-08-29 17:45:43 -0700

    Incorporate requested changes

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -44,16 +44,18 @@ export default function Search() {
       <div>
         {data &&
           data.map((r) => <SearchResult result={r as Agent} key={r.id} />)}
-        <Card body border="light" className="shadow text-center">
-          <h3 className="m-3 pt-4">Can't find what you're looking for?</h3>
-          <Button
-            aria-label="Add an agent"
-            size="lg"
-            variant="primary"
-            className="m-4 w-75 p-3 text-light"
-          >
-            Add an agent
-          </Button>
+        <Card border="0" className="text-center">
+          <Card.Body className="p-4">
+            <h3 className="mb-4">Can't find what you're looking for?</h3>
+            <Button
+              aria-label="Add an agent"
+              variant="primary"
+              size="lg"
+              className="w-75 text-white"
+            >
+              Add an agent
+            </Button>
+          </Card.Body>
         </Card>
       </div>
     </div>
