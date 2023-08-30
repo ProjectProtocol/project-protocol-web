090f79d0bd6e4e38475f2a275e5a14b9c9023c0e
Tim Sandberg <tasandberg@gmail.com>
2023-08-30 14:25:30 -0700

    Accept a borderColor prop

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,8 +1,19 @@
 import { FormControl, FormControlProps } from 'react-bootstrap'
 
+interface ISearchBar extends FormControlProps {
+  borderColor?: 'info' | 'primary' | 'success' | 'danger' | 'secondary'
+}
+
 /** Thin wrapper around Bootstrap's FormControl for consistent SearchBar style */
-export default function SearchBar({ className, ...props }: FormControlProps) {
-  const classes = ['rounded-5 border border-info border-3', className].join(' ')
+export default function SearchBar({
+  className,
+  borderColor,
+  ...props
+}: ISearchBar) {
+  const classes = [
+    className,
+    `rounded-5 border border-${borderColor || 'info'} border-3`,
+  ].join(' ')
 
   return (
     <FormControl
--- src/stories/SearchBar.stories.tsx
+++ src/stories/SearchBar.stories.tsx
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react'
 import SearchBar from 'src/components/SearchBar'
 
 const meta: Meta<typeof SearchBar> = {
-  title: 'Atoms/SearchBar',
+  title: 'Components/SearchBar',
   component: SearchBar,
   tags: ['autodocs'],
 }
@@ -12,12 +12,4 @@ type Story = StoryObj<typeof SearchBar>
 
 export const Basic: Story = {
   args: { placeholder: 'Search for agent or office' },
-}
-
-/** Accepts override props for customization */
-export const Overrides: Story = {
-  args: {
-    className: 'p-3 fs-4 border-danger',
-    placeholder: 'Search something else',
-  },
 }
