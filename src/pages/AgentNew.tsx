3b4c43bc297613092fc6da5a47ff8e8958c7982b
Eddie M. Menefee <eddie.m.menefee@gmail.com>
2023-08-30 10:32:58 -0700

    Create AgentNew page

---
+++ src/pages/AgentNew.tsx
@@ -0,0 +1,21 @@
+import { Form } from "react-hook-form"
+import { FormControl } from "react-bootstrap"
+
+
+
+
+
+
+
+export default function AgentNew() {
+
+
+    return (
+        <div>
+            <Form id="agent-new-form">
+                <FormControl 
+            />
+            </Form>
+        </div>
+    )
+}
