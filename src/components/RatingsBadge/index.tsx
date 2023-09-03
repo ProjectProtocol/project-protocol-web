48f6aeec07ae415d728f4cd7dfe03261b12751b1
Tim Sandberg <tasandberg@gmail.com>
2023-09-03 15:24:19 -0700

    add util for extracting BS them colors to JS

--- src/components/RatingsBadge/index.tsx
+++ src/components/RatingsBadge/index.tsx
@@ -1,4 +1,5 @@
-import { RATING_COLORS, svgProps } from './svgProps'
+import { svgProps } from './svgProps'
+import { ratingColors } from 'src/util/bootstrapVariables'
 
 interface IRatingsBadge {
   /** Integer or decimal rating. Can be between 0 and 5. */
@@ -10,7 +11,7 @@ export default function RatingsBadge({ rating }: IRatingsBadge) {
   const decimal = rating % 1 > 0 ? (rating % 1).toPrecision(1).slice(1) : null
   const integer = Math.floor(rating)
   const properties = svgProps
-  const fill = RATING_COLORS[integer - 1] || '#C9C9C9'
+  const fill = ratingColors[integer - 1] || '#C9C9C9'
 
   return (
     <svg {...properties.svg}>
