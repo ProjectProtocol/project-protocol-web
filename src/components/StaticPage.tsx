e476c95b59a523d514b19676ff0e58743461c8b7
eddie-m-m <eddie.m.menefee@gmail.com>
2023-09-18 15:03:59 -0700

    Add interface for static pages

--- src/components/StaticPage.tsx
+++ src/components/StaticPage.tsx
@@ -1,5 +1,10 @@
-export default function StaticPage({ title }: { title: string }) {
+interface IStaticPage {
+  title: string,
+  icon: string,
+  children: React.ReactNode
+}
 
+export default function StaticPage({title, icon, children}: IStaticPage) {
   return (
   <div>
     <div className="d-flex justify-content-center mb-3">
@@ -7,13 +12,13 @@ export default function StaticPage({ title }: { title: string }) {
         className="d-flex justify-content-center align-items-center bg-white rounded-circle"
         style={{ width: 80, height: 80 }}
         >
-        {/* set icon image and alt text depending on which static page rendered */}
-        <img src={undefined} alt="icon" width="50%" />
+        <img src={icon} alt="icon" width="50%" />
       </div>
     </div>
     <div className="p-4 text-start">
       <h2 className="mb-2">{title}</h2>
     </div>
+    <body>{children}</body>
   </div>
   )
 }
