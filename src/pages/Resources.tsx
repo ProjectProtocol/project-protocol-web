d829e99c29d3d018f463aec9ba022b54505374b6
eddie-m-m <eddie.m.menefee@gmail.com>
2023-09-22 15:13:06 -0700

    Add Resources component

---
+++ src/pages/Resources.tsx
@@ -0,0 +1,11 @@
+import icon from '../images/icon.svg'
+import StaticPage from "src/components/StaticPage"
+
+export default function Resources() {
+    
+    return (
+        <StaticPage title={"Resources"} icon={icon}>
+            <p>Resources</p>
+        </StaticPage>
+    )
+}
