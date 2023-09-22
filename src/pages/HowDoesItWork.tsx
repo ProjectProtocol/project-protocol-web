8ff5e5b61774e1af45e881b4dc26c11d7f18dc58
eddie-m-m <eddie.m.menefee@gmail.com>
2023-09-22 15:46:22 -0700

    Add How-does-it-work component

---
+++ src/pages/HowDoesItWork.tsx
@@ -0,0 +1,10 @@
+import icon from '../images/icon.svg'
+import StaticPage from 'src/components/StaticPage'
+
+export default function HowDoesItWork() {
+  return (
+    <StaticPage title={'How does it work'} icon={icon}>
+      <p>How does it work?</p>
+    </StaticPage>
+  )
+}
