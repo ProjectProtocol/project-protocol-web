152fc98bf5579878344dc2f7cf7fab9129e0f780
eddie-m-m <eddie.m.menefee@gmail.com>
2023-09-22 15:08:34 -0700

    Add FAQ component

---
+++ src/pages/Faq.tsx
@@ -0,0 +1,11 @@
+import icon from '../images/icon.svg'
+import StaticPage from "src/components/StaticPage"
+
+export default function Faq() {
+    
+    return (
+        <StaticPage title={"faq"} icon={icon}>
+            <p>faq</p>
+        </StaticPage>
+    )
+}
