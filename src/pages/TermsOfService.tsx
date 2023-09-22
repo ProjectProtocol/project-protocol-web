81a8cd080a33c0cbb39de75487a3646ab25eeb60
eddie-m-m <eddie.m.menefee@gmail.com>
2023-09-22 15:48:58 -0700

    Add Terms of Service component

---
+++ src/pages/TermsOfService.tsx
@@ -0,0 +1,10 @@
+import icon from '../images/icon.svg'
+import StaticPage from 'src/components/StaticPage'
+
+export default function TermsOfService() {
+  return (
+    <StaticPage title={'Terms of Service'} icon={icon}>
+      <p>Terms of Service</p>
+    </StaticPage>
+  )
+}
