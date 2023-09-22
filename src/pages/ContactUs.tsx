209626430a7f98495872f6abcedc8fbf8c840e4e
eddie-m-m <eddie.m.menefee@gmail.com>
2023-09-22 15:10:20 -0700

    Add Contact Us component

---
+++ src/pages/ContactUs.tsx
@@ -0,0 +1,11 @@
+import icon from '../images/icon.svg'
+import StaticPage from "src/components/StaticPage"
+
+export default function ContactUs() {
+    
+    return (
+        <StaticPage title={"contact-us"} icon={icon}>
+            <p>Contact Us</p>
+        </StaticPage>
+    )
+}
