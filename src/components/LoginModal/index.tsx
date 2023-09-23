67048e412a3c20a05661f54f5b1e5b70b6b809c2
Tim Sandberg <tasandberg@gmail.com>
2023-09-23 14:54:04 -0700

    Add registration api helper, use in login modal

--- src/components/LoginModal/index.tsx
+++ src/components/LoginModal/index.tsx
@@ -1,7 +1,7 @@
 import { Carousel, Modal, ModalBody, ModalProps } from 'react-bootstrap'
 import UserForm, { IUserFormState } from './UserForm'
 import { useAuth } from 'src/contexts/auth/AuthContext'
-import { ApiSession } from 'src/api'
+import { ApiSession, ApiUsers } from 'src/api'
 import toast from 'react-hot-toast'
 import { LOGIN_PAGES } from './constants'
 
@@ -20,7 +20,13 @@ export default function LoginModal({ page, setPage, ...props }: LoginModal) {
   }
 
   const signUp = async (data: IUserFormState) => {
-    toast.success('register: ' + JSON.stringify(data, null, 2))
+    const { user } = await ApiUsers.create(data)
+    if (user) {
+      setUser(user)
+      toast.success('Account creation successful')
+    } else {
+      toast.error('Something went wrong, please try again')
+    }
   }
 
   return (
