6956685793c9473cd8a634b7176d1902cfe8437c
Tim Sandberg <tasandberg@gmail.com>
2023-09-26 15:57:48 -0700

    Force boolean return type in confirmations util

--- src/api/confirmations.ts
+++ src/api/confirmations.ts
@@ -1,10 +1,10 @@
 import apiClient from './client'
 
 // Create a new user (register)
-export async function create({ token }: { token: string }) {
+export async function create({ token }: { token?: string }): Promise<boolean> {
   const result = await apiClient
     .post('/auth/confirmations', { token })
-    .then((r) => r.data)
+    .then((r) => 400 > r.status && r.status >= 200)
     .catch(() => false)
 
   return result
