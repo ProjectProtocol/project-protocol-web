8c11d0a1eeb5a51ab7fdf75879eef5505c2992d9
Tim Sandberg <tasandberg@gmail.com>
2023-09-27 08:53:26 -0700

    Undo changes to error page

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,4 +1,4 @@
-import { Link, useRouteError } from 'react-router-dom'
+import { useRouteError } from 'react-router-dom'
 type RoutingError = {
   statusText?: string
   message?: string
@@ -8,16 +8,12 @@ export default function ErrorPage() {
   const error = useRouteError() as RoutingError
 
   return (
-    <div
-      id="error-page"
-      className="p-5 vh-100 justify-content-center align-items-center d-flex flex-column"
-    >
+    <div id="error-page">
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
         <i>{error?.statusText || error?.message}</i>
       </p>
-      <Link to="/">Project protocol home</Link>
     </div>
   )
 }
