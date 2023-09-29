ee77362f146346001881fffbd5b2a1493f97d9a8
eddie-m-m <eddie.m.menefee@gmail.com>
2023-09-29 15:22:26 -0700

    Add contentful entry retrieval functionality to static pages

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,12 @@
+import { useLoaderData } from 'react-router-dom'
 import icon from '../images/icon.svg'
 import StaticPage from 'src/components/StaticPage'
+import { Entry } from 'contentful'
 
 export default function About() {
+  const entry = useLoaderData() as Entry
+  console.log(entry)
+
   return (
     <StaticPage title={'About'} icon={icon}>
       <p>About</p>
--- src/pages/EthicalPrinciples.tsx
+++ src/pages/EthicalPrinciples.tsx
@@ -1,10 +1,15 @@
+import { Entry } from 'contentful'
 import icon from '../images/icon.svg'
 import StaticPage from 'src/components/StaticPage'
+import { useLoaderData } from 'react-router-dom'
 
 export default function EthicalPrinciples() {
+  const entry = useLoaderData() as Entry
+  console.log(entry)
+
   return (
     <StaticPage title={'Ethical Principles'} icon={icon}>
-      <p>ethical principles</p>
+      <p>Ethical Principles</p>
     </StaticPage>
   )
 }
