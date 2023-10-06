199e592a52b5eb7b1091907196b3b433441600b0
eddie-m-m <eddie.m.menefee@gmail.com>
2023-10-05 19:28:50 -0700

    WIP

--- src/pages/HowDoesItWork.tsx
+++ src/pages/HowDoesItWork.tsx
@@ -3,10 +3,11 @@ import icon from '../images/icon.svg'
 import BasicPage from 'src/components/BasicPage'
 import { useLoaderData } from 'react-router-dom'
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
+import { Document } from '@contentful/rich-text-types'
 
 export default function HowDoesItWork() {
   const entry = useLoaderData() as Entry
-  const document = entry.fields.body
+  const document = entry.fields.body as Document
   const title = entry.fields.title as string
 
   return (
