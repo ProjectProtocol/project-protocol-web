0656e284c34398405d9760399a9a987ca46435ac
eddie-m-m <eddie.m.menefee@gmail.com>
2023-10-05 19:22:57 -0700

    WIP

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,10 +1,18 @@
 import icon from '../images/icon.svg'
 import BasicPage from 'src/components/BasicPage'
+import { useLoaderData } from 'react-router-dom'
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
+import { Entry } from 'contentful'
+import { Document } from '@contentful/rich-text-types'
 
 export default function About() {
+  const entry = useLoaderData() as Entry
+  const document = entry.fields.body as Document
+  const title = entry.fields.title as string
+
   return (
-    <BasicPage title={'About'} icon={icon}>
-      <p>About</p>
+    <BasicPage title={title} icon={icon}>
+      <div>{documentToReactComponents(document)}</div>
     </BasicPage>
   )
 }
--- src/pages/HowDoesItWork.tsx
+++ src/pages/HowDoesItWork.tsx
@@ -1,10 +1,17 @@
+import { Entry } from 'contentful'
 import icon from '../images/icon.svg'
 import BasicPage from 'src/components/BasicPage'
+import { useLoaderData } from 'react-router-dom'
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 
 export default function HowDoesItWork() {
+  const entry = useLoaderData() as Entry
+  const document = entry.fields.body
+  const title = entry.fields.title as string
+
   return (
-    <BasicPage title={'How does it work'} icon={icon}>
-      <p>How does it work?</p>
+    <BasicPage title={title} icon={icon}>
+      <div>{documentToReactComponents(document)}</div>
     </BasicPage>
   )
 }
