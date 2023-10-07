40a8aabec385030b6125805e70c9e3945ccd9281
Tim Sandberg <tasandberg@gmail.com>
2023-10-07 11:47:53 -0700

    Add fullscreen variation to BasicPage

--- src/components/BasicPage.tsx
+++ src/components/BasicPage.tsx
@@ -1,12 +1,20 @@
+import { Card } from 'react-bootstrap'
+
 interface IBasicPage {
   title: string
   icon: string
   children: React.ReactNode
+  fullScreen?: boolean
 }
 
 /** Multi-purpose page layout with a page title and icon */
-export default function BasicPage({ title, icon, children }: IBasicPage) {
-  return (
+export default function BasicPage({
+  title,
+  icon,
+  children,
+  fullScreen,
+}: IBasicPage) {
+  const inner = (
     <div className="px-4 pb-4 pt-5">
       <div className="d-flex justify-content-center mb-4">
         <div
@@ -19,5 +27,15 @@ export default function BasicPage({ title, icon, children }: IBasicPage) {
       <h2 className="mb-4 text-center">{title}</h2>
       {children}
     </div>
+  )
+
+  return fullScreen ? (
+    <div className="w-100 d-flex flex-column p-5 align-items-center vh-100 bg-light">
+      <Card style={{ top: '30%' }}>
+        <Card.Body className="p-0">{inner}</Card.Body>
+      </Card>
+    </div>
+  ) : (
+    inner
   )
 }
