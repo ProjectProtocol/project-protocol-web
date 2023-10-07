33e0d3ed9cd704aabade94db2e5c9767a87e837c
Tim Sandberg <tasandberg@gmail.com>
2023-10-07 15:58:43 -0700

    [PRO-165] Password reset landing page for changing password (#35)
    * Add hookform/error-message pkg
    
    * Delete unused file
    
    * PasswordResets page functionality
    
    * ErrorPage tweaks
    
    * Add reference link to colors.scss
    
    * Add fullscreen variation to BasicPage
    
    * Use fullscreen BasicPage for confirmation and PasswordResets.
    
    * Refactor PasswordResets page

--- src/api/passwordResets.ts
+++ src/api/passwordResets.ts
@@ -1,8 +1,38 @@
 import apiClient from './client'
 
+export async function get(token: string) {
+  const result = await apiClient
+    .get(`/auth/password_resets/${token}`)
+    .then((r) => r.status === 200)
+    .catch(() => false)
+
+  return result
+}
+
 export async function create({ email }: { email: string }) {
   const result = await apiClient
     .post('/auth/password_resets', { email })
+    .then((r) => r.data)
+    .catch(() => false)
+
+  return result
+}
+
+interface IApiPasswordResetsUpdateArgs {
+  newPassword: string
+  newPasswordConfirm: string
+  token: string
+}
+export async function update({
+  newPassword,
+  newPasswordConfirm,
+  token,
+}: IApiPasswordResetsUpdateArgs) {
+  const result = await apiClient
+    .patch(`/auth/password_resets/${token}`, {
+      newPassword,
+      newPasswordConfirm,
+    })
     .then((r) => r.data)
     .catch(() => false)
 
---
+++ src/pages/PasswordResets.tsx
@@ -0,0 +1,58 @@
+import icon from '../images/icon.svg'
+import BasicPage from 'src/components/BasicPage'
+import { useNavigate, useParams } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { ApiPasswordResets } from 'src/api'
+import toast from 'react-hot-toast'
+import PasswordResetsForm from 'src/components/PasswordResets/PasswordResetsForm'
+import SuccessModal from 'src/components/PasswordResets/SuccessModal'
+
+interface IPasswordResetsFormState {
+  newPassword: string
+  newPasswordConfirm: string
+}
+
+export default function PasswordResets() {
+  const { token } = useParams()
+  const [success, setSuccess] = useState(false)
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    async function validateToken() {
+      const isValid = await ApiPasswordResets.get(token || '')
+      if (!isValid) {
+        toast.error('Password reset has expired, please try again', {
+          id: 'invalid-pw-reset-token',
+        })
+        navigate('/', { replace: true })
+      }
+    }
+    validateToken()
+  }, [token, navigate])
+
+  const updatePassword = async ({
+    newPassword,
+    newPasswordConfirm,
+  }: IPasswordResetsFormState) => {
+    if (token === undefined) throw new Error('Invalid request')
+
+    const result = await ApiPasswordResets.update({
+      newPassword,
+      newPasswordConfirm,
+      token,
+    })
+    if (result) {
+      setSuccess(true)
+    } else {
+      toast.error('Password change failed, please try again.')
+      navigate('/', { replace: true })
+    }
+  }
+
+  return (
+    <BasicPage icon={icon} title="New password" fullScreen>
+      <PasswordResetsForm onSubmit={updatePassword} />
+      <SuccessModal show={success} />
+    </BasicPage>
+  )
+}
