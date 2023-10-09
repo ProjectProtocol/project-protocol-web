3cfd2d1cb78e0088a021f5ac4a42d0399cc490a3
Tim Sandberg <tasandberg@gmail.com>
2023-10-09 15:35:23 -0700

    Use PopUp component in AccountDelete modal and LoginModal

--- src/components/LoginModal/constants.ts
+++ src/components/LoginModal/constants.ts
@@ -1,5 +1,25 @@
+import { IPopUpTitle } from '../PopUp/PopUpTitle'
+
 export const LOGIN_PAGES = {
   SIGN_IN: 0,
   SIGN_UP: 1,
   FORGOT_PASSWORD: 2,
 }
+
+// This should return localised string keys, e.g. `loginModal.signUp.title` once i18n is
+// implemented. For now we're hardcoding strings.
+export const PAGE_TITLES: IPopUpTitle[] = [
+  {
+    title: 'Log in',
+    titleHelper: 'Create an account to share your own experiences',
+  },
+  {
+    title: 'Sign up',
+    titleHelper: 'Create an account to share your own experiences',
+  },
+  {
+    title: 'Forgot password',
+    titleHelper:
+      "Enter the email address you used to register, and we'll send you a password reset link.",
+  },
+]
