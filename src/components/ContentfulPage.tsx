231e1a0dce4fb79f533a5597103f4a50c87992f5
eddie-m-m <eddie.m.menefee@gmail.com>
2023-10-08 18:29:41 -0700

    Simplify ContentfulPage by incorporating BasicPage component

--- src/components/ContentfulPage.tsx
+++ src/components/ContentfulPage.tsx
@@ -2,6 +2,7 @@ import { Entry } from 'contentful'
 import { useLoaderData } from 'react-router-dom'
 import { Document } from '@contentful/rich-text-types'
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
+import BasicPage from './BasicPage'
 
 interface IContentfulPage {
   title: string
@@ -13,17 +14,8 @@ export default function ContentfulPage({ title, icon }: IContentfulPage) {
   const document = entry.fields.body as Document
 
   return (
-    <div className="px-4 pb-4 pt-5">
-      <div className="d-flex justify-content-center mb-4">
-        <div
-          className="d-flex justify-content-center align-items-center bg-white rounded-circle"
-          style={{ width: 80, height: 80 }}
-        >
-          <img src={icon} alt="icon" width="50%" />
-        </div>
-      </div>
-      <h2 className="mb-4 text-center">{title}</h2>
+    <BasicPage title={title} icon={icon}>
       {documentToReactComponents(document)}
-    </div>
+    </BasicPage>
   )
 }
