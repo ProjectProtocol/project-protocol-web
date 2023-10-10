5a7c06d676f694d10ca6b0b728156ceb4093f89b
Tim Sandberg <tasandberg@gmail.com>
2023-10-10 09:51:09 -0700

    Test for controlId behavior on Input

--- src/test/components/Input.test.tsx
+++ src/test/components/Input.test.tsx
@@ -4,7 +4,7 @@ import { it, expect } from 'vitest'
 import Input from 'src/components/Input'
 
 it('matches the snapshot', async () => {
-  const r = render(<Input label="My Input" name="donkey" />)
+  const r = render(<Input label="My Input" />)
   expect(r.baseElement).toMatchSnapshot()
 })
 
@@ -22,4 +22,22 @@ it('renders a text input by default', async () => {
 it('renders error message if present', async () => {
   render(<Input label="My Input" error="Hi I am an error" />)
   expect(screen.getByText('Hi I am an error')).toBeTruthy()
+})
+
+it('uses kebabCased label for id by default', async () => {
+  const { baseElement } = render(
+    <Input label="My Input" error="Hi I am an error" />,
+  )
+  expect(baseElement.querySelector('#my-input')).toBeTruthy()
+})
+
+it('accepts an explicit controlId and overrides default id', async () => {
+  const { baseElement } = render(
+    <Input
+      label="My Input"
+      error="Hi I am an error"
+      controlId={'hamster-dance1'}
+    />,
+  )
+  expect(baseElement.querySelector('#hamster-dance1')).toBeTruthy()
 })
