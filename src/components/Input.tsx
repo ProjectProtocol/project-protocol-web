4527a0cf98ddc5cb043c96df778abff042b52219
Tim Sandberg <tasandberg@gmail.com>
2023-10-10 09:47:18 -0700

    Use React.forwardRef to support any refs passed to Input

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,26 +1,25 @@
 import { kebabCase } from 'lodash'
+import React from 'react'
 import { FloatingLabel, FormControl, FormControlProps } from 'react-bootstrap'
 
 interface IInput extends FormControlProps {
   name?: string
   error?: string
   type?: 'text' | 'email' | 'password' | 'tel' | 'zip'
+  controlId?: string
   label: string
 }
 
-const Input: React.FC<IInput & FormControlProps> = ({
-  error,
-  type = 'text',
-  label,
-  ...props
-}: IInput) => {
-  const id = kebabCase(label)
+const Input: React.FC<IInput & FormControlProps> = React.forwardRef<
+  HTMLInputElement,
+  IInput
+>(({ error, controlId, type = 'text', label, ...props }, ref) => {
   return (
-    <FloatingLabel label={label} controlId={id}>
-      <FormControl {...props} type={type} placeholder={label} />
+    <FloatingLabel label={label} controlId={controlId || kebabCase(label)}>
+      <FormControl {...props} type={type} ref={ref} placeholder={label} />
       <FormControl.Feedback type="invalid">{error}</FormControl.Feedback>
     </FloatingLabel>
   )
-}
+})
 
 export default Input
