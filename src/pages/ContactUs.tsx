9792d35e7132b829d2bb03a0a62ecfe07061771f
eddie-m-m <eddie.m.menefee@gmail.com>
2023-10-15 16:58:03 -0700

    Add email link

--- src/pages/ContactUs.tsx
+++ src/pages/ContactUs.tsx
@@ -4,7 +4,11 @@ import BasicPage from 'src/components/BasicPage'
 export default function ContactUs() {
   return (
     <BasicPage title={'Contact Us'} icon={icon}>
-      <p>Contact Us</p>
+      <p>Do you have questions or suggestions?</p>
+      <div>
+        Email:
+        <a href="mailto:info@projectprotocol.org"> info@projectprotocol.org</a>
+      </div>
     </BasicPage>
   )
 }
