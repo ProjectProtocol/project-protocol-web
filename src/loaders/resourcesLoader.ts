81165c2c0eb3ac20ebeece6c8b3b7098d6b26043
Tim Sandberg <tasandberg@gmail.com>
2023-10-16 10:18:53 +0900

    Lint

--- src/loaders/resourcesLoader.ts
+++ src/loaders/resourcesLoader.ts
@@ -1,10 +1,8 @@
 import { EntryCollection } from 'contentful'
-import { LoaderFunctionArgs } from 'react-router-dom'
 import ResourceLink from 'src/types/ResourceLink'
 import ContentfulClient from 'src/util/ContentfulClient'
 
-export default async function ({} // request,
-: LoaderFunctionArgs): Promise<EntryCollection<ResourceLink>> {
+export default async function (): Promise<EntryCollection<ResourceLink>> {
   const data = await ContentfulClient.getEntries<ResourceLink>({
     content_type: 'resourceLink',
   })
