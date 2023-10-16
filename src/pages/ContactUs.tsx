99d3e8de845fd881657fcd26e6b3b334db7f1c5c
eddie-m-m <eddie.m.menefee@gmail.com>
2023-10-15 18:39:46 -0700

    Add some margin spacing to elements

--- src/pages/ContactUs.tsx
+++ src/pages/ContactUs.tsx
@@ -1,15 +1,16 @@
-import icon from '../images/icon.svg'
 import BasicPage from 'src/components/BasicPage'
+import contactEnvelope from '../images/contact-envelope.svg'
+import contactStripes from '..images/contact-stripes.svg'
 
 export default function ContactUs() {
   return (
-    <BasicPage title={'Contact Us'} icon={icon}>
-      <p>Do you have questions or suggestions?</p>
-      <div>
+    <BasicPage title={'Contact Us'} icon={contactEnvelope}>
+      <p className="mb-3">Do you have questions or suggestions?</p>
+      <div className="mb-3">
         Email:
         <a href="mailto:info@projectprotocol.org">info@projectprotocol.org</a>
       </div>
-      <div>
+      <div className="mb-3">
         Call or text:
         <a href="tel:(213)915-8585">(213) 915-8585</a>
       </div>
