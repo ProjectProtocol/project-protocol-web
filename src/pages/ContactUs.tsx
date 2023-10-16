72fcdc132f279421772a2a6f8941b5eecab2c957
eddie-m-m <eddie.m.menefee@gmail.com>
2023-10-16 11:33:33 -0700

    Add some spacing between text and links

--- src/pages/ContactUs.tsx
+++ src/pages/ContactUs.tsx
@@ -1,17 +1,16 @@
 import BasicPage from 'src/components/BasicPage'
 import contactEnvelope from '../images/contact-envelope.svg'
-// import contactStripes from '..images/contact-stripes.svg'
 
 export default function ContactUs() {
   return (
     <BasicPage title={'Contact Us'} icon={contactEnvelope}>
       <p className="mb-3">Do you have questions or suggestions?</p>
       <div className="mb-3">
-        Email:
+        <span className="me-1">Email:</span>
         <a href="mailto:info@projectprotocol.org">info@projectprotocol.org</a>
       </div>
       <div className="mb-3">
-        Call or text:
+        <span className="me-1">Call or text:</span>
         <a href="tel:(213)915-8585">(213) 915-8585</a>
       </div>
     </BasicPage>
