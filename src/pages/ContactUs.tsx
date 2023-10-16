fd9637e14281787d3d801c537d0c84e70390cf2f
Eddie M. Menefee <eddie.m.menefee@gmail.com>
2023-10-16 15:16:30 -0700

    [PRO-168] Screen: Contact Us (#40)
    * Add email link
    
    * Add phone number link
    
    * Add contact us two-piece icon
    
    * Add some margin spacing to elements
    
    * Comment out currently unused stripes icon
    
    * Add some spacing between text and links

--- src/pages/ContactUs.tsx
+++ src/pages/ContactUs.tsx
@@ -1,10 +1,18 @@
-import icon from '../images/icon.svg'
 import BasicPage from 'src/components/BasicPage'
+import contactEnvelope from '../images/contact-envelope.svg'
 
 export default function ContactUs() {
   return (
-    <BasicPage title={'Contact Us'} icon={icon}>
-      <p>Contact Us</p>
+    <BasicPage title={'Contact Us'} icon={contactEnvelope}>
+      <p className="mb-3">Do you have questions or suggestions?</p>
+      <div className="mb-3">
+        <span className="me-1">Email:</span>
+        <a href="mailto:info@projectprotocol.org">info@projectprotocol.org</a>
+      </div>
+      <div className="mb-3">
+        <span className="me-1">Call or text:</span>
+        <a href="tel:(213)915-8585">(213) 915-8585</a>
+      </div>
     </BasicPage>
   )
 }
