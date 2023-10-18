b1fb111ed5279667a54055866bcd9c4578c7aef0
Tim Sandberg <tasandberg@gmail.com>
2023-10-18 15:06:19 +0900

    Remove log msg

--- src/pages/Resources.tsx
+++ src/pages/Resources.tsx
@@ -1,21 +1,20 @@
 import { useLoaderData } from 'react-router-dom'
 import { EntryCollection } from 'contentful'
 import ResourceCard from 'src/components/ResourceCard'
-import { Card } from 'react-bootstrap'
 import { ResourceLink, ResourceLinkEntrySkeleton } from 'src/types/ResourceLink'
 
 export default function Resources() {
   const data = useLoaderData() as EntryCollection<ResourceLinkEntrySkeleton>
   const firstResource = data.items[0] as ResourceLink
   const resources = data.items.slice(1) as ResourceLink[]
-  console.log(firstResource)
+
   return (
     <div className="vertical-rhythm">
       <h2>Resources</h2>
-      <ResourceCard resource={firstResource} />
+      <ResourceCard resource={firstResource} index={3} />
       <hr />
       {resources.map((r, i) => (
-        <ResourceCard resource={r} key={`resource-card-${i}`} />
+        <ResourceCard resource={r} key={`resource-card-${i}`} index={i} />
       ))}
     </div>
   )
