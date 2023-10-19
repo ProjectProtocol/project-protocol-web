100af05e363ee4addaa41cc61a1fb375cefe98d0
Tim Sandberg <tasandberg@gmail.com>
2023-10-19 12:37:27 +0800

    Fix unused param

--- src/hooks/usePointerState.ts
+++ src/hooks/usePointerState.ts
@@ -1,4 +1,4 @@
-import { PointerEvent, useState } from 'react'
+import { useState } from 'react'
 
 // Utility hook for discerning if an element is pressed or hovered over
 export default function usePointerState() {
@@ -11,7 +11,7 @@ export default function usePointerState() {
     setHover(false)
   }
 
-  const handlePress = (e: PointerEvent<HTMLDivElement>) => {
+  const handlePress = () => {
     setPressActive(true)
   }
 
