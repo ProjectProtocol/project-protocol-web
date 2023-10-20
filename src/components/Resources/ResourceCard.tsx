010896b303f7016574c1c4ff7778ed550c403114
Tim Sandberg <tasandberg@gmail.com>
2023-10-21 07:40:14 +0800

    Use images in resource links

--- src/components/Resources/ResourceCard.tsx
+++ src/components/Resources/ResourceCard.tsx
@@ -1,10 +1,11 @@
 import { ChainModifiers, Entry } from 'contentful'
 import ListItem from '../List/ListItem'
-import { Card, Col, Placeholder, Row } from 'react-bootstrap'
+import { Badge, Card } from 'react-bootstrap'
 import { ResourceLinkSkeleton } from 'src/types/contentful-types'
 import { useEffect, useState } from 'react'
 import apiClient from 'src/api/client'
 import icon from '../../images/icon.svg'
+import resourceCategoryColor from 'src/util/resourceCategoryColor'
 
 export default function ResourceCard({
   resource,
@@ -16,14 +17,14 @@ export default function ResourceCard({
   const url = resource.fields.url as string
   const title = resource.fields.title as string
   const organization = resource.fields.organization as string
+  const category = (resource.fields.category as string[])[0]
 
   useEffect(() => {
     let ignore = false
     if (!previewImg && !ignore)
       apiClient.get('/resource_link_preview?url=' + url).then(({ data }) => {
-        if (data?.images) {
-          const imageSet = data.images
-          setPreviewImg(data.images[imageSet.length - 1]?.src)
+        if (data?.images && data.images[0]?.src) {
+          setPreviewImg(data.images[0]?.src)
         } else {
           setPreviewImg(icon)
         }
@@ -35,42 +36,30 @@ export default function ResourceCard({
 
   return (
     <ListItem onClick={() => window.open(url, '_blank')}>
-      <Row className="g-0">
-        <Col
-          xs={3}
-          className={`rounded-start d-flex align-items-center justify-content-center bg-light border overflow-hidden`}
+      <div className="position-relative">
+        <Card.Img
+          variant="top"
+          src={previewImg}
+          width="100%"
+          className="bg-dark"
+          style={{ maxHeight: '200px', objectFit: 'cover' }}
+        />
+        <Badge
+          pill
+          className={`p-2 position-absolute ${resourceCategoryColor(category)}`}
+          style={{
+            right: '1rem',
+            top: '1rem',
+          }}
         >
-          {previewImg ? (
-            // <div
-            //   className="h-100 w-100 rounded-start"
-            //   style={{
-            //     backgroundImage: `url(${previewImg})`,
-            //     backgroundRepeat: 'no-repeat',
-            //     backgroundPosition: 'center',
-            //   }}
-            // />
-            <img
-              src={previewImg}
-              style={{ width: '100%', height: '100%', objectFit: 'cover' }}
-            />
-          ) : (
-            <Placeholder
-              animation="glow"
-              className="d-flex align-items-center justify-content-center"
-            >
-              <Placeholder
-                style={{ height: '40px', width: '40px', borderRadius: '100%' }}
-              />
-            </Placeholder>
-          )}
-        </Col>
-        <Col xs={9} className="border border-start-0">
-          <Card.Body>
-            <Card.Title>{title}</Card.Title>
-            <Card.Text>{organization}</Card.Text>
-          </Card.Body>
-        </Col>
-      </Row>
+          <span className="fw-medium">{category}</span>
+        </Badge>
+      </div>
+
+      <Card.Body>
+        <Card.Title>{title}</Card.Title>
+        <Card.Text>{organization}</Card.Text>
+      </Card.Body>
     </ListItem>
   )
 }
