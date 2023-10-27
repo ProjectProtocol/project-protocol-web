b543becab6ba3fa96b4a0725db255d3803b8ff54
Tim Sandberg <tasandberg@gmail.com>
2023-10-27 09:32:14 -0700

    loader wip

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { Outlet, ScrollRestoration } from 'react-router-dom'
+import { Outlet, ScrollRestoration, useNavigation } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import Footer from '../components/Footer'
 import Menu from '../components/Menu'
@@ -6,16 +6,26 @@ import { useAuth } from 'src/contexts/auth/AuthContext'
 import LoginModal from 'src/components/LoginModal'
 import { useEffect, useState } from 'react'
 import { LOGIN_PAGES } from 'src/components/LoginModal/constants'
+import loadingBar from 'src/util/loadingBar'
 
 export default function Home() {
   const { user } = useAuth()
   const [showLogin, setShowLogin] = useState(!!user)
   const [loginPage, setLoginPage] = useState(LOGIN_PAGES.SIGN_IN)
-
+  const { state } = useNavigation()
   const openLogin = (page: number) => {
     setLoginPage(page)
     setShowLogin(true)
   }
+
+  // Global loading indicator based on router activity
+  useEffect(() => {
+    if (state === 'loading') {
+      loadingBar.show()
+    } else {
+      loadingBar.hide()
+    }
+  }, [state])
 
   useEffect(() => {
     if (user) {
