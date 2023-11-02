08b0a77191daae3c76f32c72809be0466e598664
eddie-m-m <eddie.m.menefee@gmail.com>
2023-11-02 13:26:45 -0700

    Make spinner size small

--- src/components/ChangePasswordModal.tsx
+++ src/components/ChangePasswordModal.tsx
@@ -106,6 +106,7 @@ export default function ChangePasswordModal({
             {isSubmitting ? (
               <>
                 <Spinner
+                  size="sm"
                   role="status"
                   animation="border"
                   variant="black"
