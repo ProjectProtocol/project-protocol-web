91cb6047f9057093904c241a709f3b7e31d8db22
Ariel Herman <aeh.herman@gmail.com>
2023-11-06 15:34:02 -0500

    Add i18n-backend to lazyload translation files

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -1,22 +1,11 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
+import Backend from 'i18next-http-backend'
 import LanguageDetector from 'i18next-browser-languagedetector'
-import enLocaleStrings from './en.json'
-import esLocaleStrings from './es.json'
 
-i18n
-  .use(LanguageDetector)
-  .use(initReactI18next)
-  .init({
-    fallbackLng: 'en',
-    resources: {
-      en: {
-        translation: enLocaleStrings,
-      },
-      es: {
-        translation: esLocaleStrings,
-      },
-    },
-  })
+i18n.use(Backend).use(LanguageDetector).use(initReactI18next).init({
+  fallbackLng: 'en',
+  debug: true,
+})
 
 export default i18n
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './styles/index.scss'
 import { RouterProvider } from 'react-router-dom'
+import { Spinner } from 'react-bootstrap'
 import AuthProvider from './contexts/auth/AuthProvider.tsx'
 import LocaleSwitcher from './i18n/LocaleSwitcher.tsx'
 import NotificationArea from './components/NotificationArea.tsx'
@@ -9,10 +10,12 @@ import router from './router.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AuthProvider>
-      <LocaleSwitcher />
-      <NotificationArea />
-      <RouterProvider router={router} />
-    </AuthProvider>
+    <React.Suspense fallback={<Spinner />}>
+      <AuthProvider>
+        <LocaleSwitcher />
+        <NotificationArea />
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </React.Suspense>
   </React.StrictMode>,
 )
