ec87c5be90e3f97dd5176cd8214b01b95cc6f361
Tim Sandberg <tasandberg@gmail.com>
2023-11-08 11:21:45 -0800

    Adjust SelectOfficeModal

--- src/components/SearchResult.tsx
+++ src/components/SearchResult.tsx
@@ -3,15 +3,20 @@ import Office from '../types/Office'
 import SearchResultAgent from './SearchResultAgent'
 import SearchResultOffice from './SearchResultOffice'
 import ListItem from './List/ListItem'
+import { CardProps } from 'react-bootstrap'
 
-interface ISearchResult {
+interface ISearchResult extends CardProps {
   result: Agent | Office
   onClick?: () => void
 }
 
-export default function SearchResult({ result, onClick }: ISearchResult) {
+export default function SearchResult({
+  result,
+  onClick,
+  ...cardProps
+}: ISearchResult) {
   return (
-    <ListItem onClick={onClick} body>
+    <ListItem onClick={onClick} body {...cardProps}>
       {result.type === 'Agent' ? (
         <SearchResultAgent agent={result as Agent} />
       ) : (
