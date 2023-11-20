9029b498b58031384de3ea74893af68853b4c4e5
Ariel Herman <aeh.herman@gmail.com>
2023-11-20 18:23:01 -0500

    Adds Account, Search, Vote

--- src/pages/Vote.tsx
+++ src/pages/Vote.tsx
@@ -4,19 +4,23 @@ import { Document } from '@contentful/rich-text-types'
 import renderRichText from 'src/util/renderRichText'
 import icon from '../images/icon.svg'
 import BasicPage from 'src/components/BasicPage'
+import { useTranslation } from 'react-i18next'
 
 export default function Vote() {
+  const { t } = useTranslation()
+
+  // TODO: Handle translation
   const entry = useLoaderData() as Entry
   const document = entry.fields.body as Document
 
   return (
-    <BasicPage title={'Voting rights restored in California'} icon={icon}>
+    <BasicPage title={t('vote.title')} icon={icon}>
       <div className="w-100 text-center mb-4">
         <a
           className="btn btn-primary btn-lg text-white"
           href="https://registertovote.ca.gov/"
         >
-          Register to vote
+          t('vote.register')
         </a>
       </div>
       {renderRichText(document)}
