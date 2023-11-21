c46ee29ef04987c176382430431ace241f2b96c2
Ariel Herman <aeh.herman@gmail.com>
2023-11-21 14:06:47 -0500

    Fix runtime issue

--- .storybook/i18next.ts
+++ .storybook/i18next.ts
@@ -3,32 +3,13 @@ import i18n from 'i18next';
 import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
-const ns = ['common'];
-const supportedLngs = ['en', 'es'];
-const resources = ns.reduce((acc, n) => {
-    supportedLngs.forEach((lng) => {
-    if (!acc[lng]) acc[lng] = {};
-        acc[lng] = {
-            ...acc[lng],
-            [n]: require(`../public/locales/${lng}/translation.json`),
-        };
-    });
-    return acc;
-}, {});
-
 i18n.use(initReactI18next)
     .use(LanguageDetector)
     .use(Backend)
     .init({
-        //debug: true,
         lng: 'en',
         fallbackLng: 'en',
-        defaultNS: 'common',
-        ns,
-        interpolation: {escapeValue: false},
-        react: {useSuspense: false},
-        supportedLngs,
-        resources,
+        supportedLngs: ['en', 'es'],
     });
 
 export default i18n;
