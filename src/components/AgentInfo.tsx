5d603a9e802605c8ba7e60b08e9b2910bda85058
Ariel Herman <aeh.herman@gmail.com>
2023-11-21 14:09:42 -0500

    Add missing string

--- src/components/AgentInfo.tsx
+++ src/components/AgentInfo.tsx
@@ -1,3 +1,4 @@
+import { useTranslation } from 'react-i18next'
 import Agent from '../types/Agent'
 
 interface IAgentInfo {
@@ -7,12 +8,13 @@ interface IAgentInfo {
 
 /** Repeatable UI pattern for basic agent info */
 export default function AgentInfo({ agent }: IAgentInfo) {
+  const { t } = useTranslation()
   return (
     <div className="d-flex flex-column">
       <span className="m-0 large h4 lh-sm">
         {agent.lastName}, {agent.firstName}
       </span>
-      <span className="mb-1 text-tertiary h4 lh-sm">Agent</span>
+      <span className="mb-1 text-tertiary h4 lh-sm">{t('agent.agent')}</span>
       <p className="m-0 lh-sm">{agent.office.street}</p>
       <p className="m-0 lh-sm">
         {agent.office.city}, {agent.office.state} {agent.office.zip}
