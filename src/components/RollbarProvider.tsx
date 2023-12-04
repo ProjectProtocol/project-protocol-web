2080b5dbda5da62e5b439d7f259d97fe8cc4b41e
Tim Sandberg <tasandberg@gmail.com>
2023-12-04 10:42:25 -0800

    wip

--- src/components/RollbarProvider.tsx
+++ src/components/RollbarProvider.tsx
@@ -4,7 +4,19 @@ import { Configuration } from 'rollbar'
 const rollbarConfig: Configuration = {
   accessToken: '193b4f35ab424d7ba68b09ca6a046067',
   environment: import.meta.env.VITE_ENVIRONMENT || import.meta.env.MODE,
+  captureUncaught: true,
+  captureUnhandledRejections: true,
   enabled: import.meta.env.PROD,
+  payload: {
+    environment: import.meta.env.VITE_ENVIRONMENT,
+    client: {
+      javascript: {
+        source_map_enabled: true,
+        code_version: process.env.COMMIT_REF,
+        guess_uncaught_frames: true,
+      },
+    },
+  },
 }
 
 export default function RollbarProvider({
--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,35 @@ import pwaManifestConfig from './manifest.config.js'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import { defineConfig } from 'vitest/dist/config.js'
 
+import viteRollbar from 'vite-plugin-rollbar'
+
+// other declarations
+
+/*
+  Configure Rollbar plugin
+*/
+const rollbarConfig = {
+  accessToken: process.env.SOURCEMAP_ACCESS_TOKEN, // d17af97925844fbb94168560484dc300
+  version: '1.0',
+  baseUrl: process.env.HOSTNAME || 'localhost',
+  ignoreUploadErrors: false,
+  silent: false,
+}
+
 // References:
 // https://vitejs.dev/config/
 // https://vite-pwa-org.netlify.app/assets-generator/#example-using-minimal-preset
 export default defineConfig({
-  plugins: [tsconfigPaths(), react(), VitePWA(pwaManifestConfig)],
+  plugins: [
+    tsconfigPaths(),
+    react(),
+    VitePWA(pwaManifestConfig),
+    viteRollbar(rollbarConfig),
+  ],
+  build: {
+    // Required: tells Vite to create source maps
+    sourcemap: true,
+  },
   test: {
     globals: true,
     environment: 'jsdom',
