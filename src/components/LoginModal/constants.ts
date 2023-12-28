7baeb31afd040d31727a92639c4d96cd57e6846d
eddie-m-m <eddie.m.menefee@gmail.com>
2023-12-28 13:49:17 -0800

    Update strings in constants

--- src/components/LoginModal/constants.ts
+++ src/components/LoginModal/constants.ts
@@ -2,22 +2,27 @@ export const LOGIN_PAGES = {
   SIGN_IN: 0,
   SIGN_UP: 1,
   FORGOT_PASSWORD: 2,
+  CONFIRM_SIGNUP: 3,
 }
 
 // This should return localised string keys, e.g. `loginModal.signUp.title` once i18n is
 // implemented. For now we're hardcoding strings.
-export const PAGE_TITLES = [
-  {
-    title: 'Project Protocol',
-    titleHelper: 'Login to your account',
-  },
-  {
-    title: 'Sign up',
-    titleHelper: 'Create an account to rate your parole officer',
-  },
-  {
-    title: 'Forgot password',
-    titleHelper:
-      "Enter the email address you used to register, and we'll send you a password reset link.",
-  },
-]
+// export const PAGE_TITLES = [
+//   {
+//     title: 'Project Protocol',
+//     titleHelper: 'Login to your account',
+//   },
+//   {
+//     title: 'Sign up for Project Protocol',
+//     titleHelper: 'Create an account to rate your parole officer',
+//   },
+//   {
+//     title: 'Forgot password',
+//     titleHelper:
+//       "Enter the email address you used to register, and we'll send you a password reset link.",
+//   },
+//   {
+//     title: 'Check your email to finish signing up',
+//     titleHelper: '',
+//   },
+// ]
