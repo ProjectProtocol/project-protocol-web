0fde2dc85d95d7898dbf7b451dfef7786a1b3c01
eddie-m-m <eddie.m.menefee@gmail.com>
2023-12-28 13:42:38 -0800

    Update strings in ConfirmSignup

--- src/components/LoginModal/ConfirmSignup.tsx
+++ src/components/LoginModal/ConfirmSignup.tsx
@@ -11,11 +11,11 @@ export default function ConfirmSignup({ email }: IConfirmSignup) {
     <div className="d-block p-4">
       <div>
         <p className="mb-4">
-          {t('account.loginModal.confirmDetail1', {
+          {t('account.loginModal.loginConfirmSignupDetail1', {
             email: <strong>{email}</strong>,
           })}
         </p>
-        <p>{t('account.loginModal.confirmDetail2')}</p>
+        <p>{t('account.loginModal.loginConfirmSignupDetail2')}</p>
       </div>
     </div>
   )
