e7cc2b82073987ba36801c2a51b3b59d8b4bc291
Tim Sandberg <tasandberg@gmail.com>
2024-01-07 09:52:06 -0800

    Reuse confirmation modal when unconfirmed user tries to add agent

--- src/components/AddAgentCard.tsx
+++ src/components/AddAgentCard.tsx
@@ -1,46 +1,66 @@
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
 import { useTranslation } from 'react-i18next'
-import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { useLogin } from 'src/contexts/LoginUIProvider/LoginUIContext'
 import { useAuth } from 'src/contexts/auth/AuthContext'
 import { LOGIN_PAGES } from './LoginModal/constants'
+import { useState } from 'react'
+import ConfirmationModal from './ConfirmationModal'
 
 export default function AddAgentCard() {
   const { t } = useTranslation()
-  const { isSignedIn } = useAuth()
+  const navigate = useNavigate()
+  const { user } = useAuth()
   const { openLogin } = useLogin()
+  const [showConfirmModal, setShowConfirmModal] = useState(false)
 
   return (
-    <Card border="0" className="text-center mb-3">
-      <Card.Body className="p-4">
-        <h3 className="mb-4">{t('search.noResults')}</h3>
-        {isSignedIn ? (
-          <Link
-            to="/agents/new"
-            aria-label={t('search.addAnAgent')}
-            className="w-75 btn btn-lg btn-primary"
-          >
-            {t('search.addAnAgent')}
-          </Link>
-        ) : (
-          <div className="text-center w-100 d-flex flex-column justify-content-center align-items-center">
+    <>
+      <Card border="0" className="text-center mb-3">
+        <Card.Body className="p-4">
+          <h3 className="mb-4">{t('search.noResults')}</h3>
+          {user ? (
             <Button
-              onClick={() => openLogin(LOGIN_PAGES.SIGN_UP, '/agents/new')}
-              aria-label={t('search.signUpToAddAgent')}
-              className="w-75 btn btn-lg btn-primary d-block"
+              onClick={() =>
+                user.isConfirmed
+                  ? navigate('/agents/new')
+                  : setShowConfirmModal(true)
+              }
+              aria-label={t('search.addAnAgent')}
+              className="w-75 btn btn-lg btn-primary"
             >
-              {t('search.signUpToAddAgent')}
+              {t('search.addAnAgent')}
             </Button>
-            <Button
-              variant="link"
-              onClick={() => openLogin(LOGIN_PAGES.SIGN_IN, '/agents/new')}
-            >
-              {t('search.orLogIn')}
-            </Button>
-          </div>
-        )}
-      </Card.Body>
-    </Card>
+          ) : (
+            <div className="text-center w-100 d-flex flex-column justify-content-center align-items-center">
+              <Button
+                onClick={() => openLogin(LOGIN_PAGES.SIGN_UP, '/agents/new')}
+                aria-label={t('search.signUpToAddAgent')}
+                className="w-75 btn btn-lg btn-primary d-block"
+              >
+                {t('search.signUpToAddAgent')}
+              </Button>
+              <Button
+                variant="link"
+                onClick={() => openLogin(LOGIN_PAGES.SIGN_IN, '/agents/new')}
+              >
+                {t('search.orLogIn')}
+              </Button>
+            </div>
+          )}
+        </Card.Body>
+      </Card>
+      {user && (
+        <ConfirmationModal
+          show={showConfirmModal}
+          onHide={() => setShowConfirmModal(false)}
+          title={t('search.confirmAccountToAddAgent')}
+          bodyClass="px-4"
+          user={user}
+          closeButton
+        />
+      )}
+    </>
   )
 }
