80a7cca1ec8cf8496e45de8124f28d3dcb482a6b
Tim Sandberg <tasandberg@gmail.com>
2024-01-15 16:59:18 -0800

    [PRO-223] Push sourcemaps to rollbar (#89)

--- vite.config.ts
+++ vite.config.ts
@@ -4,25 +4,48 @@ import { VitePWA } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react'
 import pwaManifestConfig from './manifest.config.js'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { loadEnv } from 'vite'
 import { defineConfig } from 'vitest/dist/config.js'
+import viteRollbar from 'vite-plugin-rollbar'
 
 // References:
 // https://vitejs.dev/config/
 // https://vite-pwa-org.netlify.app/assets-generator/#example-using-minimal-preset
-export default defineConfig({
-  plugins: [tsconfigPaths(), react(), VitePWA(pwaManifestConfig)],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: ['src/test/setup.ts'],
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: false,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  const plugins = [tsconfigPaths(), react(), VitePWA(pwaManifestConfig)]
+
+  if (env.ROLLBAR_ACCESS_TOKEN) {
+    plugins.push(
+      viteRollbar({
+        accessToken: env.ROLLBAR_ACCESS_TOKEN,
+        baseUrl: `https://${env.HOSTNAME}`,
+        version: '1.0',
+        ignoreUploadErrors: true,
+        silent: false,
+      }),
+    )
+  }
+
+  return {
+    build: {
+      sourcemap: true,
+    },
+    plugins,
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: ['src/test/setup.ts'],
+    },
+    server: {
+      proxy: {
+        '/api': {
+          target: 'http://localhost:3000',
+          changeOrigin: false,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
       },
     },
-  },
+  }
 })
