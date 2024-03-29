52c28508d27a67b2d67321dd8ee4ee9039c45637
Tim Sandberg <tasandberg@gmail.com>
2024-03-29 15:42:52 -0700

    Move toasts to bottom for smaller screens

--- src/components/NotificationArea.tsx
+++ src/components/NotificationArea.tsx
@@ -1,10 +1,13 @@
 import { Toaster } from 'react-hot-toast'
+import useWindowSize from 'src/hooks/useWindowSize'
 
 // Options: https://react-hot-toast.com/docs/toaster
 export default function NotificationArea() {
+  const [width] = useWindowSize()
+
   return (
     <Toaster
-      position="top-center"
+      position={width < 768 ? 'bottom-center' : 'top-center'}
       toastOptions={{
         className: 'text-white',
         style: {
