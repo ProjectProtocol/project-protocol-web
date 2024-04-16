69c8caada4b05d3fcf96f947fc448a06bc361048
Yuki O'Brien <613711+yuuks@users.noreply.github.com>
2024-04-16 15:33:18 -0400

    Adds UI update when voting on resource in ResourceView
    Adds back button

--- src/pages/ResourceView.tsx
+++ src/pages/ResourceView.tsx
@@ -1,10 +1,31 @@
-import { useLoaderData } from 'react-router-dom'
+import { useState } from 'react'
+import { useTranslation } from 'react-i18next'
+import { useLoaderData, useNavigate } from 'react-router-dom'
+import ResourceCard from 'src/components/Resources/ResourceCard'
+import { ResourceLoaderReturn } from 'src/loaders/resourceLoader'
+import Resource from 'src/types/Resource'
 
 export default function ResourceView() {
-  const { resource } = useLoaderData()
+  const navigate = useNavigate()
+  const { t } = useTranslation()
+  const data = useLoaderData() as ResourceLoaderReturn
+  const [resource, setResource] = useState(data.resource)
+  const onUpdateResource = (updatedResourceData: { resource: Resource }) => {
+    const updatedResource = updatedResourceData.resource
+    const newResource = { ...resource, ...updatedResource }
+
+    setResource(newResource)
+  }
+
   return (
-    <div className="vertical-rhythm">
-      <h1>Resource</h1>
+    <div>
+      <a role="button" onClick={() => navigate(-1)}>
+        <i className="bi bi-chevron-left align-middle" />
+        {t('ui.back')}
+      </a>
+      <div className="vertical-rhythm">
+        <ResourceCard resource={resource} onUpdate={onUpdateResource} />
+      </div>
     </div>
   )
 }
