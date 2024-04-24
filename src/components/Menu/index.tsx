8104c08b45e1c371e395c66be1cebfee2b2c4262
Tim Sandberg <tasandberg@gmail.com>
2024-04-24 12:03:14 -0700

    Menu update

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,14 +1,11 @@
 import { Button, Container, Nav, Navbar } from 'react-bootstrap'
-import { useLocation, useNavigate } from 'react-router-dom'
+import { NavLink, useNavigate } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
 import icon from '../../images/icon.svg'
-import MobileMenu from './MobileMenu'
-import MenuLinks from './MenuLinks'
 import User from 'src/types/User'
-import useWindowSize from 'src/hooks/useWindowSize'
-import { useEffect, useState } from 'react'
 import { useLogin } from 'src/contexts/LoginUIProvider/LoginUIContext'
 import LocaleSwitcher from 'src/components/LocaleSwitcher'
+import { LOGIN_PAGES } from '../LoginModal/constants'
 
 interface IMenu {
   user?: User
@@ -18,72 +15,86 @@ export default function Menu({ user }: IMenu) {
   const { t } = useTranslation()
   const { openLogin } = useLogin()
   const navigate = useNavigate()
-  const size = useWindowSize()
-  const location = useLocation()
-  const [showDrawer, setShowDrawer] = useState(false)
-  const openDrawer = () => setShowDrawer(true)
-  const closeDrawer = () => setShowDrawer(false)
-
-  useEffect(() => {
-    if (location) {
-      closeDrawer()
-    }
-  }, [location, size])
 
   return (
-    <Navbar className="bg-white flex-column py-0" sticky="top">
-      <div className="w-100 bg-light">
+    <Navbar className="bg-black flex-column py-0 overflow-hidden" sticky="top">
+      {/* Desktop locale switcher */}
+      <div
+        className="w-100 d-none d-md-block"
+        style={{ background: 'rgba(255,255,255,0.15)' }}
+      >
         <Container style={{ maxWidth: 935 }}>
           <div className="d-flex flex-row justify-content-end align-items-center py-2">
-            <LocaleSwitcher />
+            <LocaleSwitcher dark />
           </div>
         </Container>
       </div>
-      <Container style={{ maxWidth: 935 }} className="py-2">
-        <Navbar.Brand onClick={() => navigate('')}>
-          <div
-            className="d-flex flex-row justify-content-center align-items-center"
-            role="button"
-          >
-            <img
-              src={icon}
-              width="30"
-              height="30"
-              className="me-1"
-              alt={t('ui.ppLogoAlt')}
-            />
-            <span
-              className="fs-2 w-100 d-none d-md-inline fw-medium pe-auto text-body"
-              style={{ letterSpacing: -0.5 }}
-            >
-              Project Protocol
-            </span>
-          </div>
-        </Navbar.Brand>
-        <span
-          className="fs-2 w-100 text-center d-md-none fw-medium"
-          style={{ letterSpacing: -0.5 }}
+      <div
+        className="w-100 d-block bg-black"
+        style={{
+          boxShadow: '0 0 10px 0 rgba(0,0,0,0.5)',
+        }}
+      >
+        <Container
+          style={{ maxWidth: 935 }}
+          className="py-2 d-flex justify-content-between align-items-center"
         >
-          Project Protocol
-        </span>
-        <Nav className="fs-4 d-none d-md-flex align-items-center">
-          <MenuLinks isSignedIn={!!user} openLogin={openLogin} />
-        </Nav>
-        <div className="d-md-none">
-          <Button variant="link" onClick={openDrawer}>
-            <i className="bi bi-list text-body fs-2" />
-          </Button>
-          <MobileMenu onHide={closeDrawer} show={showDrawer}>
-            <MenuLinks
-              isSignedIn={!!user}
-              openLogin={(page: number) => {
-                closeDrawer()
-                openLogin(page)
-              }}
-            />
-          </MobileMenu>
-        </div>
-      </Container>
+          <Navbar.Brand onClick={() => navigate('')}>
+            <div
+              className="d-flex flex-row justify-content-center align-items-center"
+              role="button"
+            >
+              <img
+                src={icon}
+                width="28"
+                height="28"
+                className="me-2"
+                alt={t('ui.ppLogoAlt')}
+              />
+              <span
+                className="fs-2 w-100 d-md-inline fw-medium pe-auto text-white"
+                style={{ letterSpacing: -0.5 }}
+              >
+                Project Protocol
+              </span>
+            </div>
+          </Navbar.Brand>
+          <Nav className="fs-4 d-none d-md-flex align-items-center text-white">
+            <Nav.Link as={NavLink} className="link-white m-0" to="/">
+              {t('home.title')}
+            </Nav.Link>
+            <Nav.Link as={NavLink} className="link-white m-0" to="/rate-my-po">
+              {t('navigation.searchOfficers')}
+            </Nav.Link>
+            <Nav.Link as={NavLink} className=" link-white m-0" to="resources">
+              {t('navigation.resources')}
+            </Nav.Link>
+            {user ? (
+              <Nav.Link
+                as={NavLink}
+                className=" link-white m-0"
+                to="/account"
+                title={t('navigation.account')}
+              >
+                <i className="bi bi-person-circle align-middle" />
+              </Nav.Link>
+            ) : (
+              <Nav.Link>
+                <Button
+                  onClick={() => {
+                    openLogin(LOGIN_PAGES.SIGN_UP)
+                  }}
+                >
+                  {t('navigation.signUp')}
+                </Button>
+              </Nav.Link>
+            )}
+          </Nav>
+          <Nav className="d-md-none">
+            <LocaleSwitcher dark />
+          </Nav>
+        </Container>
+      </div>
     </Navbar>
   )
 }
