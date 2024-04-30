77f32238c0a469271c2eee29bb29f5d89391af1b
Tim Sandberg <tasandberg@gmail.com>
2024-04-30 16:46:53 -0700

    Fix password reset form/screen

--- src/components/FullScreenLayout.tsx
+++ src/components/FullScreenLayout.tsx
@@ -1,5 +1,6 @@
 import { Container } from 'react-bootstrap'
 import Card from 'react-bootstrap/Card'
+import icon from '../images/icon.svg'
 
 /**
  * Full-screen layout for simple pages outside of the normal APP ui.
@@ -12,11 +13,16 @@ export default function FullScreenLayout({
   children: React.ReactNode
 }) {
   return (
-    <div className="bg-light min-vh-100 d-flex flex-column">
-      <Container className="p-3 d-flex d-column justify-content-center align-items-center">
+    <div className="bg-light min-vh-100 d-flex flex-column align-items-center justify-content-center">
+      <Container style={{ maxWidth: 600 }}>
         <Card>
-          <Card.Body>
-            {title && <Card.Title>{title}</Card.Title>}
+          <Card.Body className="vertical-rhythm">
+            <h5>
+              <span className="d-flex align-items-start">
+                <img src={icon} width="20" className="me-2" />
+                {title}
+              </span>
+            </h5>
             {children}
           </Card.Body>
         </Card>
