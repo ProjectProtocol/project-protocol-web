126e3f7f35c0bc305374fce77140205739cdd6bd
Tim Sandberg <tasandberg@gmail.com>
2024-05-04 00:24:15 -0700

    BackendFetch to get strings from cdn in prod

--- src/contexts/TranslationsProvider/tolgee.ts
+++ src/contexts/TranslationsProvider/tolgee.ts
@@ -3,6 +3,7 @@ import {
   DevTools,
   LanguageStorage,
   LanguageDetector,
+  BackendFetch,
 } from '@tolgee/react'
 import { FormatIcu } from '@tolgee/format-icu'
 
@@ -11,10 +12,16 @@ const tolgee = Tolgee()
   .use(FormatIcu())
   .use(LanguageDetector())
   .use(LanguageStorage())
+  .use(
+    BackendFetch({
+      prefix: 'https://cdn.tolg.ee/39569eaae5eec38d77a0693bbe71ce67',
+    }),
+  )
   .init({
     availableLanguages: ['en', 'es-MX'],
     defaultLanguage: 'en',
     fallbackLanguage: 'en',
+    fallbackNs: 'shared',
     apiUrl: import.meta.env.VITE_APP_TOLGEE_API_URL,
     apiKey: import.meta.env.VITE_APP_TOLGEE_API_KEY,
   })
