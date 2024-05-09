d7657157fbdfc4e70be604488a23b69850ffde8a
Yuki O'Brien <613711+yuuks@users.noreply.github.com>
2024-05-09 13:37:55 -0400

    Use PageHeader component for back button in ResourceView

--- src/pages/ResourceView.tsx
+++ src/pages/ResourceView.tsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import { Button, Card, FormControl } from 'react-bootstrap'
 import { useTranslation } from 'react-i18next'
-import { useLoaderData, useNavigate } from 'react-router-dom'
+import { useLoaderData } from 'react-router-dom'
 import { ApiResources } from 'src/api'
 import { IResourceCommentParams } from 'src/api/resources'
 import AnimatedList from 'src/components/AnimatedList'
 import Divider from 'src/components/Divider'
+import PageHeader from 'src/components/PageHeader'
 import ResourceCard from 'src/components/Resources/ResourceCard'
 import ResourceComment from 'src/components/Resources/ResourceComment'
 import SendIcon from 'src/components/svg/Send'
@@ -15,7 +16,6 @@ import Resource from 'src/types/Resource'
 import bootstrapVariables from 'src/util/bootstrapVariables'
 
 export default function ResourceView() {
-  const navigate = useNavigate()
   const { t } = useTranslation()
   const data = useLoaderData() as ResourceLoaderReturn
   const [resource, setResource] = useState(data.resource)
@@ -51,56 +51,53 @@ export default function ResourceView() {
   ]
 
   return (
-    <div className="vertical-rhythm">
-      <div>
-        <a role="button" onClick={() => navigate(-1)}>
-          <i className="bi bi-chevron-left align-middle" />
-          {t('ui.back')}
-        </a>
-      </div>
-      <ResourceCard resource={resource} onUpdate={onUpdateResource} />
-      <Divider />
-      <div className="vertical-rhythm-sm">
-        <Card body>
-          <div className="position-relative">
-            <FormControl
-              className="bg-light border-0 shadow-none"
-              as="textarea"
-              value={commentText}
-              onChange={(e) => setCommentText(e.target.value)}
-              placeholder={t('resources.comments.add')}
-              rows={1}
-            />
-            <Button
-              variant="link"
-              className="p-0 text-dark position-absolute d-flex align-items-center"
-              style={{
-                right: '1rem',
-                bottom: '0',
-                height: '38px',
-              }}
-              disabled={submitDisabled}
-              onClick={() => onSubmit({ body: commentText })}
-            >
-              <SendIcon
-                fill={
-                  submitDisabled
-                    ? bootstrapVariables['mediumGray']
-                    : bootstrapVariables['cobalt']
-                }
+    <>
+      <PageHeader title={''} showBack />
+      <div className="vertical-rhythm">
+        <ResourceCard resource={resource} onUpdate={onUpdateResource} />
+        <Divider />
+        <div className="vertical-rhythm-sm">
+          <Card body>
+            <div className="position-relative">
+              <FormControl
+                className="bg-light border-0 shadow-none"
+                as="textarea"
+                value={commentText}
+                onChange={(e) => setCommentText(e.target.value)}
+                placeholder={t('resources.comments.add')}
+                rows={1}
               />
-            </Button>
-          </div>
-        </Card>
-        <AnimatedList>
-          {dummyComments.map((comment, index) => (
-            <ResourceComment
-              comment={comment}
-              key={`resource-comment-${index}`}
-            />
-          ))}
-        </AnimatedList>
+              <Button
+                variant="link"
+                className="p-0 text-dark position-absolute d-flex align-items-center"
+                style={{
+                  right: '1rem',
+                  bottom: '0',
+                  height: '38px',
+                }}
+                disabled={submitDisabled}
+                onClick={() => onSubmit({ body: commentText })}
+              >
+                <SendIcon
+                  fill={
+                    submitDisabled
+                      ? bootstrapVariables['mediumGray']
+                      : bootstrapVariables['cobalt']
+                  }
+                />
+              </Button>
+            </div>
+          </Card>
+          <AnimatedList>
+            {dummyComments.map((comment, index) => (
+              <ResourceComment
+                comment={comment}
+                key={`resource-comment-${index}`}
+              />
+            ))}
+          </AnimatedList>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
